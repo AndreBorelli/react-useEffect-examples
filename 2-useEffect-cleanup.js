4e3c4f56b45a2d79f8c3c3f904a2d33db4030360
AndreBorelli <71100458+AndreBorelli@users.noreply.github.com>
2022-06-20 14:47:36 -0300

    Add files via upload

---
+++ 2-useEffect-cleanup.js
@@ -0,0 +1,36 @@
+// In this example, we assign an empty array as the second parameter of useEffect,
+// so that it doesn't overload the application with too many addEventListener. In this case, as it is a small 
+// application, it does not make much difference, but in larger projects, this can have an impact on its 
+// efficiency.
+
+
+import React, { useState, useEffect } from 'react';
+
+// cleanup function
+// second argument
+
+const UseEffectCleanup = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log('useEffect');
+    window.addEventListener('resize', checkSize);
+    return () => {
+      console.log('cleanup');
+      window.removeEventListener('resize', checkSize);
+    };
+  }, []);
+  console.log('render');
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
+};
+
+export default UseEffectCleanup;
